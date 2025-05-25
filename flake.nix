507dead82f87571fe8fff77827958e475d332bc6
Linus Dikomey <l.dikomey03@gmail.com>
2025-05-25 17:17:23 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    nixpkgsFor = system: import nixpkgs {inherit system;};
+  in {
+    packages = forAllSystems (system: let
+      pkgs = nixpkgsFor system;
+    in {
+      waldbot = pkgs.buildGoModule {
+        pname = "waldbot";
+        version = "0.1.0";
+        src = ./.;
+        vendorHash = "sha256-/SSobuox1+YG2tWLMfbO1cFp3E9kd/5Mw11GOWA4SR0=";
+        buildInputs = [];
+      };
+      default = self.packages.${system}.waldbot;
+    });
+  };
+}
